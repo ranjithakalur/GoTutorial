88ab939995da5850e963e93a068b2bc6e2467edc
Skellington <wagunerusan@gmail.com>
2017-08-09 21:47:59 -0300

    Code update to test new package's functions.

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	fmt.Printf("Hello, World!\nor\nMarry fucking Xmas, fellas!\n")
+	fmt.Printf("Hello, World! Vamos ver quantos núcleos de processamento a tua máquina tem?\n")
+	var x string
+	fmt.Printf("Digite \"Sim\", para prosseguir.\n")
+	fmt.Scan(&x)
+	if x == "sim" {
+		n := runtime.NumCPU()
+		fmt.Printf("Este computador tem %v cores.\n", n)
+	} else {
+		fmt.Printf("Que peninha! Adeus! (Chato do caralho!)\n")
+	}
 }
